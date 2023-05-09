bcc148250d3261eb6faa8e9008e1687de3b10bd1
Camila Lopes <camilalopesqta@gmail.com>
2023-05-09 09:40:43 -0300

    alteração dos exercícios de pŕática guiada

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,12 @@
 // Resolva o Exercício de Fixação!
+
+const verificaIdade = (nome, anoNascimento, anoAtual) => {
+        let idade = (anoAtual - anoNascimento)
+        let maiorIdade = idade >= 18
+    return (`${nome} é maior de idade? ${idade} ${maiorIdade}`)
+}
+
+console.log(verificaIdade(`Camila`, 1982, 2023))
+console.log(verificaIdade(`Elza`, 1954, 2023))
+console.log(verificaIdade(`Arthur`, 2016, 2023))
+
